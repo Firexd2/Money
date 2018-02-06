a6b99d1c2e9a74f7cecf983d86b888006740985b
Firexd2 <firedenis@yandex.ru>
2018-02-06 12:50:17 +0600

    Development of template #1

--- Core/urls.py
+++ Core/urls.py
@@ -20,5 +20,6 @@ from Core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
     path('new/', views.create_configuration)
 ]
--- Core/views.py
+++ Core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+def home(request):
+    return render(request, 'home.html', locals())
+
+
 def create_configuration(request):
 
-    return render(request, 'new_configuration.html', locals())
+    return render(request, 'new.html', locals())
