6b928cfc99c4c701addb749849a004807baf36af
Firexd2 <firedenis@yandex.ru>
2018-02-08 15:26:12 +0300

    development new configuration

--- Core/static/js/scripts.js
+++ Core/static/js/scripts.js
@@ -21,11 +21,10 @@ $(document).ready(function() {
     });
     onNavbar();
 
-
     function procent(value) {
         var amount = parseInt($('input[name=incom]').val());
         if (amount && value) {
-            return (value / (amount / 100)).toFixed(2)
+            return Math.round(value / (amount / 100))
         } else {
             if (amount) {
                 return 0
@@ -37,20 +36,28 @@ $(document).ready(function() {
 
     function Total() {
         var n = 0;
-        var p = 0;
         var inputs = $('.cat');
+        var amount = parseInt($('input[name=incom]').val());
+        var tr = $('#total-tr');
         for (var i=0; i<inputs.length; i++) {
             if (parseInt(inputs.eq(i).val())) {
                 n += parseInt(inputs.eq(i).val());
             }
-            p += parseInt(inputs.eq(i).parent().next().children().text());
         }
-        if (!(isNaN(p))) {
-            $('#p').text(p);
-            $('#n').text(n);
+        if ($('input[name=incom]').val()) {
+            $('#p').text(100 - Math.ceil(n / (amount / 100)));
+            $('#o').text(amount - n);
+            if (n === amount) {
+                tr.css({'background': 'rgba(28, 255, 0, 0.35)'})
+            } else if (n > amount) {
+                tr.css({'background': 'rgba(255, 7, 0, 0.31)'})
+            } else {
+                tr.css({'background': 'none'})
+            }
         } else {
             $('#p').text('-- ');
-            $('#n').text('-- ');
+            $('#o').text('--');
+            tr.css({'background': 'none'})
         }
     }
 
@@ -69,7 +76,6 @@ $(document).ready(function() {
         } else {
             $('.ca').attr("disabled", true);
             $('.cat').attr("disabled", true);
-
         }
     }
 
@@ -84,12 +90,48 @@ $(document).ready(function() {
             }
             MaxProcent();
         });
-        $('.cat').on('input', function () {
+
+        $('body').on('input', '.cat', function () {
             MaxProcent();
+        });
+
+        $('#add-category').on('click', function () {
+            var tr = $('tr');
+            var incom = $('input[name=incom]').val();
+            var disabled = '';
+            var proc = '0';
+            if (!(incom)) {
+                disabled = 'disabled';
+                proc = '-- '
+            }
+            tr.eq(tr.length-4).after('<tr>\n' +
+                '<td><b>' + (tr.length-3) + '</b></td>\n' +
+                '<td><input ' + disabled +  ' name="name-cat' + (tr.length-3) + '" type="text" class="form-control ca" placeholder="Пример: На еду"/></td>\n' +
+                '<td><input ' + disabled +  ' name="incom' + (tr.length-3) + '" type="number" class="form-control cat"/></td>\n' +
+                '<td style="text-align: center"><span>' + proc + '</span>%</td>\n' +
+                '</tr>');
+        });
+
+        $('#remove-category').on('click', function () {
+            $('tr').eq($('tr').length-4).remove();
+            Total()
+        });
+
+        $('form').on('submit', function (e) {
+
+            if ($('#o').text() !== '0') {
+                e.preventDefault();
+                if (!($('#name-input').val())) {
+                    $('#error-form').text(' Вы не ввели название конфигурации')
+                } else if ($('#o').text() === '--') {
+                    $('#error-form').text(' Вы не ввели и не распределили сумму')
+                } else if (parseInt($('#o').text()) > 0) {
+                    $('#error-form').text(' У вас остался неиспользованный остаток')
+                } else {
+                    $('#error-form').text(' Вы превысили лимит вашей суммы')
+                }
+            }
         })
-
-
     }
     New();
-
 });
